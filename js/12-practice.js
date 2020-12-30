bfce8248a4ba9f59fc4286ea989e2f533eeed1cb
Karen Ruiz <anakaren.ruizb@gmail.com>
2020-12-30 13:03:30 -0600

    conditional branching

---
+++ js/12-practice.js
@@ -0,0 +1,7 @@
+let correctAnswer = prompt("What's the 'official' name of JavaScript?");
+
+if (correctAnswer == "ECMAScript") {
+    alert("Right!");
+}else{
+    alert("You don't know? ECMAScript!");
+}
