aa1f46170353e2450bd2d084dd19b71598d2dd17
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-01-05 02:10:30 -0600

    loops

---
+++ js/19-practice.js
@@ -0,0 +1,5 @@
+let i = 3;
+
+while (i) { // last value is 1, stops the loop when i = 0
+    alert(i--);
+}
---
+++ js/22-practice.js
@@ -0,0 +1,5 @@
+let userNumber;
+
+do {
+    userNumber = prompt('Enter a number greater than 100?', 0);
+} while (userNumber <= 100 && userNumber);
