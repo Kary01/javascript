e5992f97223ee1a47236cd5b7a7f278f750a790d
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-01-29 00:09:04 -0600

    handling errors

--- js/44-practice.js
+++ js/44-practice.js
@@ -3,24 +3,30 @@ const PEOPLE_URL = 'people/:id/';
 const CALL_API = {crossDomain: true};
 
 
-function callback(value, call){
+function loadCall(value, callback){
     const ID_URL = `${API_URL}${PEOPLE_URL.replace(':id', value)}`;
 
-    $.get(ID_URL, CALL_API, function(people){
-        alert(`Hola soy ${people.name}`);
-
-        if (call) {
-            call();
-        };
+    $.get(ID_URL, CALL_API, callback).fail(function(){
+            console.log(`Lo sentimos no se encontro el personaje`);
     });
 };
 
 
-callback(1, function(){
-    callback(2, function(){
-        callback(3, function(){
-            callback(4, function(){
-                callback(5);
+loadCall(1, function(people){
+    console.log(`Hola soy ${people.name}`);
+
+    loadCall(2, function(people){
+        console.log(`Hola soy ${people.name}`);
+
+        loadCall(3, function(people){
+            console.log(`Hola soy ${people.name}`);
+
+            loadCall(4, function(people){
+                console.log(`Hola soy ${people.name}`);
+
+                loadCall(5, function(people){
+                    console.log(`Hola soy ${people.name}`);
+                });
             });
         });
     });
