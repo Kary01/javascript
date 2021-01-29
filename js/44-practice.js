4a89e1e4fa84047f04e7e15b8e66efa574616eb9
Karen Ruiz <anakaren.ruizb@gmail.com>
2021-01-28 23:15:33 -0600

    CallbackHell

--- js/44-practice.js
+++ js/44-practice.js
@@ -1,22 +1,27 @@
 const API_URL = 'https://swapi.dev/api/';
 const PEOPLE_URL = 'people/:id/';
-
-
 const CALL_API = {crossDomain: true};
 
-function callback(value){
-    // console.log(arguments); ....  parameters
-    
-    alert(`Hola soy ${value.name}`);
+
+function callback(value, call){
+    const ID_URL = `${API_URL}${PEOPLE_URL.replace(':id', value)}`;
+
+    $.get(ID_URL, CALL_API, function(people){
+        alert(`Hola soy ${people.name}`);
+
+        if (call) {
+            call();
+        };
+    });
 };
 
-function people(id){
-    const ID_URL = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
 
-    $.get(ID_URL, CALL_API, callback);
-};
-
-for (let i = 1; i <= 5; i++) {
-    people(i);
-}
-
+callback(1, function(){
+    callback(2, function(){
+        callback(3, function(){
+            callback(4, function(){
+                callback(5);
+            });
+        });
+    });
+});
